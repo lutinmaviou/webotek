ca67a4193c3a1eaaddc31913f6da79b40952b14c
lutinmaviou <bertrandbou@hotmail.fr>
2020-01-14 13:45:01 +0100

    refactor(view): change routing for index as home

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -7,15 +7,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
-    /**
-     * @Route("/library", name="library")
-     */
-    public function index()
-    {
-        return $this->render('library/index.html.twig', [
-            'controller_name' => 'LibraryController',
-        ]);
-    }
 
     /**
      * @Route("/", name="home")
