2ab2ca61c46d3f44a998348a3d4a60b9371d78f6
lutinmaviou <bertrandbou@hotmail.fr>
2020-01-14 14:02:39 +0100

    feat(view): create books.html.twig for list books in db

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +15,17 @@ class LibraryController extends AbstractController
     public function home()
     {
         return $this->render('library/home.html.twig');
+    }
+
+    /**
+     * @Route("/books", name="books_list")
+     */
+    public function list()
+    {
+        $repo = $this->getDoctrine()->getRepository(Book::class);
+        $books = $repo->findAll();
+        return $this->render('library/booksList.html.twig', [
+            'books' => $books
+        ]);
     }
 }
