f5cbb4a0297997f23cc531fc740dbe46ab5254b7
lutinmaviou <bertrandbou@hotmail.fr>
2020-01-16 13:25:56 +0100

    feat(User): add password hash before entrance in db

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -118,7 +118,7 @@ class User
 
     public function setPassword(string $password): self
     {
-        $this->password = $password;
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
 
         return $this;
     }
