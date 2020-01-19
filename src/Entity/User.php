14c56ce12a627691f2753c5d74a1e7ad15b364db
lutinmaviou <bertrandbou@hotmail.fr>
2020-01-19 14:48:16 +0100

    feat(home): say hello to connected user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="json")
      */
-    private $roles = [];
+    private $roles = ['ROLE_USER'];
 
     /**
      * @var string The non hashed password
