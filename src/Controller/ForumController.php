fe5feb2e0c22b460f4827e13ea3e3ae930edf389
lutinmaviou <bertrandbou@hotmail.fr>
2020-02-11 18:52:20 +0100

    refactor(comments): add a token system for deleting comment with a form

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -99,14 +99,16 @@ class ForumController extends AbstractController
     }
 
     /**
-     * @Route("/forum/delete/{slug}/{commentId}", name="app_forum_remove_comment")
+     * @Route("/forum/delete/{slug}/{commentId}", name="app_forum_remove_comment", methods="DELETE")
      */
-    public function removeComment(int $commentId, string $slug, EntityManagerInterface $em)
+    public function removeComment(int $commentId, string $slug, EntityManagerInterface $em, Request $request)
     {
         $comment = $this->getDoctrine()->getRepository(Comment::class)->find($commentId);
-        dump($comment);
-        $em->remove($comment);
-        $em->flush();
+        dd($comment->getId());
+        if ($this->isCsrfTokenValid('delete' . $comment->getId(), $request->get('_token'))) {
+            //$em->remove($comment);
+            //$em->flush();
+        }
         return $this->redirectToRoute('app_forum_display', [
             'slug' => $slug,
         ]);
