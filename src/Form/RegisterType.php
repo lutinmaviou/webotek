a98c529d563f80693f9a190c35284a6850ff93ab
lutinmaviou <bertrandbou@hotmail.fr>
2020-02-25 19:53:10 +0100

    feat(sign-in form): add a password confirmation field

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotNull;
@@ -19,7 +20,11 @@ class RegisterType extends AbstractType
             // ->add('lastName')
             ->add('pseudo')
             ->add('email')
-            ->add('password', PasswordType::class);
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer mot de passe'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
